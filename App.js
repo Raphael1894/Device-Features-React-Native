6e38ea77b60e928ec9d1b41dd2d9268221721028
Raphael1894 <rapha.pereira.perso@gmail.com>
2022-06-22 11:00:57 +0200

    Add location with maps & necessary packages

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
+import Map from "./screens/Map";
 import IconButton from "./components/ui/IconButton";
 import { Colors } from "./constants/colors";
-
 
 const Stack = createNativeStackNavigator();
 
@@ -16,21 +16,34 @@ export default function App() {
     <Fragment>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerStyle: {backgroundColor: Colors.primary500}, headerTintColor: Colors.gray700,
-          contentStyle: {backgroundColor: Colors.gray700}
-        }}>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
           <Stack.Screen
             name="AllPlaces"
             component={AllPlaces}
-            options={({navigation}) => ({
-              title: 'Your favorite places',
+            options={({ navigation }) => ({
+              title: "Your favorite places",
               headerRight: ({ tintColor }) => (
-                <IconButton icon={"add"} size={24} color={tintColor} onPress={() => navigation.navigate('AddPlace')}/>
+                <IconButton
+                  icon={"add"}
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
               ),
             })}
           />
-          <Stack.Screen name="AddPlace" options={{title: 'Add a new place'}} component={AddPlace} />
+          <Stack.Screen
+            name="AddPlace"
+            options={{ title: "Add a new place" }}
+            component={AddPlace}
+          />
+          <Stack.Screen name="Map" component={Map}/>
         </Stack.Navigator>
       </NavigationContainer>
     </Fragment>
--- screens/Map.js
+++ screens/Map.js
@@ -1,7 +1,74 @@
-import { Text } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { Alert, StyleSheet } from "react-native";
+import { useCallback, useLayoutEffect, useState } from "react";
+import IconButton from "../components/ui/IconButton";
 
-function Map() {
-  return <Text>Map</Text>;
+function Map({ navigation }) {
+  const [selectedLocation, setLocatedLocation] = useState();
+
+  const region = {
+    latitude: 37.78,
+    longitude: -122.43,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
+  function selectLocationHandler(event) {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+    setLocatedLocation({ lat: lat, lng: lng });
+  }
+
+  const savedPickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert(
+        "No location picked",
+        "You have to select a location by clicking on the map first"
+      );
+      return;
+    }
+    navigation.navigate("AddPlace", {
+      pickedLat: selectedLocation.lat,
+      pickedLng: selectedLocation.lng,
+    });
+  }, [navigation, selectedLocation]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon="save"
+          size={24}
+          color={tintColor}
+          onPress={savedPickedLocationHandler}
+        />
+      ),
+    });
+  }, [navigation, savedPickedLocationHandler]);
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
