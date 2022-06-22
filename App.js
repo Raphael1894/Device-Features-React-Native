b860f6ce0fa76a4bcf8c99b700334070cb45ec5c
Raphael1894 <rapha.pereira.perso@gmail.com>
2022-06-22 17:46:38 +0200

    database init & splashscreen

--- App.js
+++ App.js
@@ -1,19 +1,56 @@
 import { StatusBar } from "expo-status-bar";
-import { Fragment } from "react";
+import { Fragment, useEffect, useCallback, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as SplashScreen from "expo-splash-screen";
 
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import Map from "./screens/Map";
 import IconButton from "./components/ui/IconButton";
 import { Colors } from "./constants/colors";
+import { init } from "./util/database";
+import { View } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+
+        await init();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+        console.log("appIsReady");
+      }
+    }
+    console.log("prepare");
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    async function hideSplash() {
+      if (appIsReady) {
+        console.log("await");
+
+        await SplashScreen.hideAsync();
+      }
+    }
+    hideSplash();
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <Fragment>
+    <>
       <StatusBar style="dark" />
       <NavigationContainer>
         <Stack.Navigator
@@ -43,9 +80,9 @@ export default function App() {
             options={{ title: "Add a new place" }}
             component={AddPlace}
           />
-          <Stack.Screen name="Map" component={Map}/>
+          <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
-    </Fragment>
+    </>
   );
 }
